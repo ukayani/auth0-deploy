9d353e1a5935f73d6103db319c947d7fb1a89b18
Umair Kayani <ukayani@loyalty.com>
2017-01-22 13:51:52 -0500

    - created script entry point to run deployment logic for each component type - fixed issue with connection update (removed strategy property)

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,73 @@
-/**
- * Created by ukayani on 2017-01-21.
- */
+#!/usr/bin/env node
+const pkg = require('../package.json');
+const co = require('bluebird').coroutine;
+const path = require('path');
+const program = require('commander');
+const nconf = require('nconf');
+const assert = require('assert');
+const ManagementClient = require('auth0').ManagementClient;
+const deploy = require('../lib/deploy');
+const token = require('../lib/token');
+
+const fail = (message) => {
+  console.error(message);
+  process.exit(1);
+};
+
+const hasToken = options => options.token;
+const hasClient = options => options.clientId && options.clientSecret;
+
+const checkRequired = options => {
+  if (!options.auth0Domain) fail('domain is required');
+  if (!hasToken(options) && !hasClient(options)) fail('need to specify one of token or (client-id, client-secret)');
+};
+
+const getClientInfo = (options) => {
+    return {
+      id: options.clientId,
+      secret: options.clientSecret
+    };
+};
+
+const run = (type, options) => {
+  return co(function* () {
+    nconf.argv().env();
+
+    const token = (hasToken(options)) ? options.token: yield token.get(options.auth0Domain, getClientInfo(options));
+    const client = new ManagementClient({
+      token,
+      domain: options.auth0Domain
+    });
+
+    const workingDir = options.workingDir ? path.resolve(options.workingDir) : process.cwd();
+
+    deploy(client, type, workingDir, nconf);
+
+  })();
+};
+
+const setupProgram = (program, type, description) => {
+  program
+    .command(type)
+    .allowUnknownOption(true)
+    .description(description)
+    .action(() => {
+      checkRequired(program);
+      run(type, program);
+    });
+};
+
+program
+  .version(pkg.version)
+  .allowUnknownOption(true)
+  .option('-t, --token <token>', 'Auth0 Management API token')
+  .option('-c, --client-id <id>', 'Auth0 Client ID')
+  .option('-s, --client-secret <secret>', 'Auth0 Client Secret')
+  .option('-d, --auth0-domain <domain>', 'Auth0 Domain')
+  .option('-w, --working-dir <workingdir>', 'working directory for auth0 components');
+
+setupProgram(program, 'resource', 'Create resource servers');
+setupProgram(program, 'connection', 'Create connections');
+setupProgram(program, 'client', 'Create clients');
+
+program.parse(process.argv);
